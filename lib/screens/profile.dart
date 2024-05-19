34fe35a9cd4a192c84b005844627aa2024e4a4f4
Ye Htet Kyaw <mgyehtetkyaw@gmail.com>
2024-05-20 04:46:26 +0700

    profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -5,9 +5,82 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    width: screenSize.width * 0.3,
+                    height: screenSize.height * 0.1,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      child: Icon(
+                        color: Colors.grey.shade100,
+                        Icons.person,
+                        size: 50,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: screenSize.width * 0.01,
+                  ),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Yell Htet Kyaw',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text(
+                        'level 1',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: screenSize.width * 0.1,
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.settings))
+                ],
+              ),
+              SizedBox(
+                height: screenSize.height * 0.01,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                ),
+              ),
+              SizedBox(
+                height: screenSize.height * 0.01,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Your Fav Items',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: const Text(
+                      'show all',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
